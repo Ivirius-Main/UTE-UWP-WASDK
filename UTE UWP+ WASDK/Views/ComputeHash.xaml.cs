342ec9fac591b03303e0ec953439bb325ebd2e6a
jpb <jakub.baranski.33@gmail.com>
2024-05-25 20:52:02 +0100

    Add project files.

---
+++ UTE UWP+ WASDK/Views/ComputeHash.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using UTE_UWP_.Helpers;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UTE_UWP_.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ComputeHash : Page
+    {
+        public ComputeHash()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainPage mainPage = (App.Window.Content as Frame).Content as MainPage;
+            string docText = mainPage.docText;
+            docText = EncryptorsDecryptors.Base64Encode(docText);
+            base64_result.Text = docText;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainPage mainPage = (App.Window.Content as Frame).Content as MainPage;
+            string docText = mainPage.docText;
+            docText = EncryptorsDecryptors.Base64Decode(docText);
+            base64_result.Text = docText;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainPage mainPage = (App.Window.Content as Frame).Content as MainPage;
+            string docText = mainPage.docText;
+            docText = EncryptorsDecryptors.SHA1Encrypt(docText);
+            sha1_result.Text = docText;
+        }
+    }
+}
---
+++ UTE UWP+ WASDK/Views/SettingsPage.xaml.cs
@@ -0,0 +1,294 @@
+﻿//using MicaForUWP.Media;
+using Microsoft.Graphics.Canvas.Text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+using UTE_UWP_.Helpers;
+using UTE_UWP_.Services;
+using UTE_UWP_.Views;
+
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Storage;
+using Microsoft.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace UTE_UWP_.Views
+{
+    // TODO: Add other settings as necessary. For help see https://github.com/microsoft/TemplateStudio/blob/main/docs/UWP/pages/settings-codebehind.md
+    // TODO: Change the URL for your privacy policy in the Resource File, currently set to https://YourPrivacyUrlGoesHere
+    public sealed partial class SettingsPage : Page, INotifyPropertyChanged
+    {
+        private ElementTheme _elementTheme = ThemeSelectorService.Theme;
+
+        public ElementTheme ElementTheme
+        {
+            get { return _elementTheme; }
+
+            set { Set(ref _elementTheme, value); }
+        }
+
+        private string _versionDescription;
+
+        public string VersionDescription
+        {
+            get { return _versionDescription; }
+
+            set { Set(ref _versionDescription, value); }
+        }
+
+        public List<string> accentcolors = new List<string>
+        {
+            "Default",
+            //"Windows 10 Blue",
+            "Slate Green",
+            "Lilac"
+        };
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+            ((SystemAccentColorSetting)Application.Current.Resources["SystemAccentColorSetting"]).SystemAccentColor = new SolidColorBrush(Colors.Red);
+
+
+            //if (BuildInfo.BeforeWin11)
+            //{
+            //    if (App.Window.RequestedTheme == ApplicationTheme.Light)
+            //    {
+            //        Application.Current.Resources["AppTitleBarBrush"] = new BackdropMicaBrush()
+            //        {
+            //            LuminosityOpacity = 0.8F,
+            //            TintOpacity = 0F,
+            //            BackgroundSource = BackgroundSource.WallpaperBackdrop,
+            //            Opacity = 1,
+            //            TintColor = Windows.UI.Color.FromArgb(255, 230, 230, 230),
+            //            FallbackColor = Windows.UI.Color.FromArgb(255, 230, 230, 230)
+            //        };
+            //        this.Background = (Brush)Application.Current.Resources["AppTitleBarBrush"];
+            //    }
+            //    else
+            //    {
+            //        Application.Current.Resources["AppTitleBarBrush"] = new BackdropMicaBrush()
+            //        {
+            //            LuminosityOpacity = 0.8F,
+            //            TintOpacity = 0F,
+            //            BackgroundSource = BackgroundSource.WallpaperBackdrop,
+            //            Opacity = 1,
+            //            TintColor = Windows.UI.Color.FromArgb(255, 25, 25, 25),
+            //            FallbackColor = Windows.UI.Color.FromArgb(25, 25, 25, 25)
+            //        };
+            //        this.Background = (Brush)Application.Current.Resources["AppTitleBarBrush"];
+            //    }
+
+            //}
+            //else
+            //{
+
+            //}
+
+            // TODO Windows.UI.ViewManagement.ApplicationView is no longer supported. Use Microsoft.UI.Windowing.AppWindow instead. For more details see https://docs.microsoft.com/en-us/windows/apps/windows-app-sdk/migrate-to-windows-app-sdk/guides/windowing
+            //var appViewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            //appViewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            //appViewTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            //var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            //coreTitleBar.ExtendViewIntoTitleBar = true;
+            //UpdateTitleBarLayout(coreTitleBar);
+
+            //App.Window.SetTitleBar(AppTitleBar);
+
+            //coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+            //coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+
+            var LocalSettings = ApplicationData.Current.LocalSettings;
+
+            if ((string)LocalSettings.Values["AccentTheme"] == "Slate Green")
+            {
+                AccentBox.SelectedItem = "Slate Green";
+            }
+            if ((string)LocalSettings.Values["AccentTheme"] == "Lilac")
+            {
+                AccentBox.SelectedItem = "Lilac";
+            }
+            if ((string)LocalSettings.Values["AccentTheme"] == "Default")
+            {
+                AccentBox.SelectedItem = "Default";
+            }
+        }
+
+        //private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        //{
+        //    UpdateTitleBarLayout(sender);
+        //}
+
+        //private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        //{
+        //    AppTitleBar.Visibility = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+        //}
+
+        //// Update the TitleBar based on the inactive/active state of the app
+        //private void Current_Activated(object sender, WindowActivatedEventArgs e)
+        //{
+        //    SolidColorBrush defaultForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorPrimaryBrush"];
+        //    SolidColorBrush inactiveForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorDisabledBrush"];
+
+        //    if (e.WindowActivationState == CoreWindowActivationState.Deactivated)
+        //    {
+        //        AppTitle.Foreground = inactiveForegroundBrush;
+        //    }
+        //    else
+        //    {
+        //        AppTitle.Foreground = defaultForegroundBrush;
+        //    }
+        //}
+
+        //private void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        //{
+        //    // Update title bar control size as needed to account for system size changes.
+        //    AppTitleBar.Height = coreTitleBar.Height;
+
+        //    // Ensure the custom title bar does not overlap window caption controls
+        //    Thickness currMargin = AppTitleBar.Margin;
+        //    AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+        //}
+
+        //private void BackButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (App.Window.Content is Frame rootFrame && rootFrame.CanGoBack)
+        //    {
+        //        rootFrame.GoBack();
+        //    }
+        //}
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            VersionDescription = GetVersionDescription();
+            await Task.CompletedTask;
+        }
+
+        private string GetVersionDescription()
+        {
+            var appName = "AppDisplayName".GetLocalized();
+            var package = Package.Current;
+            var packageId = package.Id;
+            var version = packageId.Version;
+
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        private async void ThemeChanged_CheckedAsync(object sender, RoutedEventArgs e)
+        {
+            var param = (sender as RadioButton)?.CommandParameter;
+
+            if (param != null)
+            {
+                await ThemeSelectorService.SetThemeAsync((ElementTheme)param);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        public List<string> Fonts;
+
+        #region Appearance
+        public int DocumentViewPadding;
+
+        public string DefaultFont;
+
+        // Modes:
+        // 0. No wrap
+        // 1. Wrap
+        // 2. Wrap whole words
+
+        public int TextWrapping;
+
+        // Modes:
+        // 0. Light
+        // 1. Dark
+        // 2. Default
+
+        public int Theme;
+        #endregion
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private async void GH_Navigate(object sender, RoutedEventArgs e)
+        {
+            // The URI to launch
+            string uriToLaunch = @"https://github.com/jpbandroid/UTE-UWP-Plus";
+
+            // Create a Uri object from a URI string 
+            var uri = new Uri(uriToLaunch);
+
+            // Launch the URI
+            async void DefaultLaunch()
+            {
+                // Launch the URI
+                var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+
+                if (success)
+                {
+                    // URI launched
+                }
+                else
+                {
+                    // URI launch failed
+                }
+            }
+            DefaultLaunch();
+        }
+
+        private void VIDsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.Window.Content is Frame rootFrame)
+            {
+                rootFrame.Navigate(typeof(VelocityIDsPage));
+            }
+        }
+
+        private void AccentBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var LocalSettings = ApplicationData.Current.LocalSettings;
+            if (AccentBox.SelectedItem != null)
+            {
+                if ((string)AccentBox.SelectedItem == "Default")
+                {
+                    LocalSettings.Values["AccentTheme"] = "Default";
+                } else if ((string)AccentBox.SelectedItem == "Slate Green")
+                {
+                    LocalSettings.Values["AccentTheme"] = "Slate Green";
+                } else if ((string)AccentBox.SelectedItem == "Lilac")
+                {
+                    LocalSettings.Values["AccentTheme"] = "Lilac";
+                }
+            }   
+        }
+    }
+
+}
